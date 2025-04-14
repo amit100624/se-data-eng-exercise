d0e6bfd5f0a501e9dce04c01fd1ce312cd3b80cc
Amit Dash <amit.dash@equalexperts.com>
2025-04-14 18:24:01 +0530

    Add incremental logic in dim_location

--- airflow/dbt_taxi_trips/models/dim_location.sql
+++ airflow/dbt_taxi_trips/models/dim_location.sql
@@ -2,13 +2,15 @@ WITH TAXI_TRIPS_LOCATIONS AS (
     SELECT
         {{ dbt_utils.generate_surrogate_key(['PICKUP_LATITUDE', 'PICKUP_LONGITUDE']) }} AS LOCATION_ID,
         PICKUP_LATITUDE AS LATITUDE,
-        PICKUP_LONGITUDE AS LONGITUDE
+        PICKUP_LONGITUDE AS LONGITUDE,
+        CREATED_TIMESTAMP AS UPDATED_TS
     FROM {{ ref('taxi_trips_consistent') }}
     UNION
     SELECT
         {{ dbt_utils.generate_surrogate_key(['DROPOFF_LATITUDE', 'DROPOFF_LONGITUDE']) }} AS LOCATION_ID,
         DROPOFF_LATITUDE AS LATITUDE,
-        DROPOFF_LONGITUDE AS LONGITUDE
+        DROPOFF_LONGITUDE AS LONGITUDE,
+        CREATED_TIMESTAMP AS UPDATED_TS
     FROM {{ ref('taxi_trips_consistent') }}
 )
 
@@ -16,7 +18,12 @@ SELECT * FROM (
     {{ dbt_utils.deduplicate(
          relation = 'TAXI_TRIPS_LOCATIONS',
          partition_by = 'LOCATION_ID',
-         order_by = 'LATITUDE'
+         order_by = 'UPDATED_TS DESC'
          )
-    }}
+    }})
+{% if is_incremental() %}
+WHERE UPDATED_TS > (
+    SELECT COALESCE(MAX(UPDATED_TS), '{{ var('default_date') }}')
+    FROM {{ this }}
 )
+{% endif %}
