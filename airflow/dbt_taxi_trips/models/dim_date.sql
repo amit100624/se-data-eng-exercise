6bda97a9906be4cf1d94e80850ebe5006ad6a76c
Amit Dash <amit.dash@equalexperts.com>
2025-04-14 18:42:27 +0530

    Add incremental logic in dim_date

--- airflow/dbt_taxi_trips/models/dim_date.sql
+++ airflow/dbt_taxi_trips/models/dim_date.sql
@@ -1,17 +1,20 @@
 WITH TAXI_TRIPS_DATES AS (
     SELECT
-        DATE(TPEP_PICKUP_DATETIME)::TIMESTAMP_NTZ AS DATE
+        DATE(TPEP_PICKUP_DATETIME)::TIMESTAMP_NTZ AS DATE,
+        CREATED_TIMESTAMP AS UPDATED_TS
     FROM {{ ref('taxi_trips_consistent') }}
     UNION
     SELECT
-        DATE(TPEP_DROPOFF_DATETIME)::TIMESTAMP_NTZ AS DATE
+        DATE(TPEP_DROPOFF_DATETIME)::TIMESTAMP_NTZ AS DATE,
+        CREATED_TIMESTAMP AS UPDATED_TS
     FROM {{ ref('taxi_trips_consistent') }}
 ),
 
 TAXI_TRIPS_DATES_WITH_KEYS AS (
     SELECT
         {{ dbt_utils.generate_surrogate_key(['DATE']) }} AS DATE_ID,
-        DATE
+        DATE,
+        UPDATED_TS
     FROM TAXI_TRIPS_DATES
 )
 
@@ -20,12 +23,18 @@ SELECT
     YEAR(DATE)::NUMBER(4,0) AS YEAR,
     MONTH(DATE)::NUMBER(2,0) AS MONTH,
     WEEK(DATE)::NUMBER(2,0) AS WEEK_OF_THE_YEAR,
-    DATE(DATE)::TIMESTAMP_NTZ AS DATE
+    DATE(DATE)::TIMESTAMP_NTZ AS DATE,
+    UPDATED_TS
 FROM (
     {{ dbt_utils.deduplicate(
        relation = 'TAXI_TRIPS_DATES_WITH_KEYS',
        partition_by = 'DATE_ID',
-       order_by = 'DATE'
+       order_by = 'UPDATED_TS DESC'
        )
-    }}
+    }})
+{% if is_incremental() %}
+WHERE UPDATED_TS > (
+    SELECT COALESCE(MAX(UPDATED_TS), '{{ var('default_date') }}')
+    FROM {{ this }}
 )
+{% endif %}
