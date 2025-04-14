79493a8b0439fab7f3027f1363352ee5346bfbe3
Amit Dash <amit.dash@equalexperts.com>
2025-04-14 18:54:10 +0530

    Add incremental logic in dim_vendor

--- airflow/dbt_taxi_trips/models/dim_vendor.sql
+++ airflow/dbt_taxi_trips/models/dim_vendor.sql
@@ -4,5 +4,12 @@ SELECT
         WHEN VENDOR_ID = '1' THEN 'Creative Mobile Technologies, LLC'
         WHEN VENDOR_ID = '2' THEN 'VeriFone Inc'
         ELSE 'N/A'
-    END AS VENDOR_NAME
+    END AS VENDOR_NAME,
+    CREATED_TIMESTAMP AS UPDATED_TS
 FROM {{ ref('taxi_trips_consistent') }}
+{% if is_incremental() %}
+WHERE UPDATED_TS > (
+    SELECT COALESCE(MAX(UPDATED_TS), '{{ var('default_date') }}')
+    FROM {{ this }}
+)
+{% endif %}
